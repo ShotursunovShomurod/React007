a230da312f60ef8196869d156ad1a73f146fb364
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-08-14 09:36:53 +0500

    heroni togirlab qoydim

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,33 +4,38 @@ import img from "../../assets/hero.png";
 const Hero = () => {
   return (
     <>
-      <div className="mt-32 ">
-        <div className="container mx-auto">
-          <div className="flex items-center justify-center gap-20	">
-            <div className="flex flex-col gap-8 items-start">
+    <div className="mt-32">
+        <div className="container">
+          <div
+            id="Hero"
+            className="flex items-center justify-center gap-2 flex-wrap"
+          >
+            <div className="flex flex-col gap-8 items-start w-[470px]">
               <h2 className="text-5xl font-bold w-4/6">
                 Мебель на любой вкус!
               </h2>
-              <ul className="flex flex-col list-disc	text-lg font-normal">
-                <li className="hover:text-yellow-500 cursor-pointer">
+              <ul className="flex flex-col list-disc text-lg font-normal">
+                <li className="marker:text-yellow-300">
                   Худи, чашки для горячего чая и термосы{" "}
                 </li>
-                <li className="hover:text-yellow-500 cursor-pointer	">
+                <li className="marker:text-yellow-300">
                   Eлочные игрушки, брелочки
                 </li>
-                <li className="hover:text-yellow-500 cursor-pointer">
+                <li className="marker:text-yellow-300">
                   Начало списка вещей, которые можно{" "}
                 </li>
               </ul>
-              <button className="hover:bg-[yellowgreen] py-2.5 px-20	bg-amber-500 rounded-3xl text-white	">
+              <button className="py-2.5 px-20 bg-amber-500 rounded-3xl text-white hover:bg-yellow-500 duration-300">
                 Перейти в каталог
               </button>
-              <p className="hover:text-[yellowgreen] cursor-pointer items-center text-center w-3/6 text-amber-500">
-                Внести список артикулов
-              </p>
+              <a href="#">
+                <p className="items-center flex text-center text-amber-500">
+                  Внести список артикулов
+                </p>
+              </a>
             </div>
-            <div>
-              <img src={img} alt="" />
+            <div className="w-[500px] h-[350px]">
+              <img className="w-full h-full" src={img} alt="Hero" />
             </div>
           </div>
         </div>
