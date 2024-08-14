95629c977f085a41f442b70127ab932635c618f7
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-08-14 10:18:14 +0500

    container qoshib qoydim

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -6,8 +6,8 @@ import { useNavigate } from 'react-router-dom';
 const NotFound = () => {
     const navigate = useNavigate()
   return (
-    <div className="text-center">
-        <img className='inline-block w-full h-[600px]' src={Error} alt="" />
+    <div className="text-center container mx-auto">
+        <img className='inline-block w-full mx-auto h-[600px]' src={Error} alt="" />
         <button className='border p-3 font-bold cursor-pointer' onClick={()=> navigate("/")}>Go Home</button>
     </div>
   );
